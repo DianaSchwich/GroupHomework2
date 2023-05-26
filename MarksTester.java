9f818180ede92511696855950e5152cb7335bfd0
Diana Schwichtenberg <diana963sde@gmail.com>
2023-05-26 16:02:59 -0400

    first commit

---
+++ MarksTester.java
@@ -0,0 +1,18 @@
+package HWgroup2;
+
+public class MarksTester {
+    // Testing the code
+    public class MarksTest {
+        public static void main(String[] args) {
+            // Creating student A object
+            Marks studentA = new A(80, 90, 70);
+            double percentageA = studentA.getPercentage();
+            System.out.println("Student A Percentage: " + percentageA + "%");
+
+            // Creating student B object
+            Marks studentB = new B(75, 85, 80, 90);
+            double percentageB = studentB.getPercentage();
+            System.out.println("Student B Percentage: " + percentageB + "%");
+        }
+    }
+}
---
+++ Task3.java
@@ -0,0 +1,27 @@
+package HWgroup2;
+
+import class18.Vehicle;
+
+/*
+Count the Number of Words in a String: Write a function to count the
+number of words in a given string. Words are separated by spaces or
+punctuation. For example, the input "Hello, world!" should return 2.
+
+ */
+public class Task3 {
+    public static int countWords(String str) {
+        if (str.isEmpty()) {
+            return 0;
+        }
+
+        String[] words = str.split("\\s+");
+        return words.length;
+    }
+
+    public static void main(String[] args) {
+        String input = "Hello, world!";
+        int wordCount = countWords(input);
+        System.out.println( wordCount);
+    }
+
+}
